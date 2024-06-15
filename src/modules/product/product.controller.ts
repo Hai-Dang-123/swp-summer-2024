f382d0e75655e51c67618fc1d73ea285da2e2499
ltrungg <trungagchw@gmail.com>
2024-06-15 15:16:11 +0700

    sua lai phan product

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -33,7 +33,8 @@ export class ProductController {
   //  @Render('buy/buy')
   getBuy() { }
 
-  @Get('related/:id') //56c06978-b984-44f9-aff6-ee03a0da0787
+  @Get('related/:id') 
+  //56c06978-b984-44f9-aff6-ee03a0da0787
   // @Render('product/product-detail')  
   findRelatedProducts(@Param('id') id: string) {
     return this.productService.findRelatedProducts(id);
